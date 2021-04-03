25ac10d80e7c48b687a47fcc858e1a6dbc248df7
RailGun15 <ignaciokeegan@gmail.com>
2021-04-03 14:19:35 -0300

    Agregue los puntos 4 y 5 de agustin y los puntos 6 y 7 de ignacio keegan

--- TP1_Grupo7_L4/src/ejercicio1/Empleado.java
+++ TP1_Grupo7_L4/src/ejercicio1/Empleado.java
@@ -8,6 +8,22 @@ public class Empleado {
     private String nombre;
     private int edad;
     
+    private static int numeroEmpleado = 999;
+    
+    public Empleado() {
+    	numeroEmpleado++;
+    	this.id = numeroEmpleado;
+    	this.nombre = "sin nombre";
+    	this.edad = 99;
+    }
+    
+    public Empleado(String nombre, int edad) {
+    	numeroEmpleado++;
+    	this.id = numeroEmpleado;
+    	this.nombre = nombre;
+    	this.edad = edad;
+    }
+    
     //Getters and Setters
     
 	public int getId() {
@@ -25,7 +41,16 @@ public class Empleado {
 	public int getEdad() {
 		return edad;
 	}
+	@Override
+	public String toString() {
+		return "Empleado ID " + id + ", se llama " + nombre + " de " + edad + " años de edad" ;
+	}
+
 	public void setEdad(int edad) {
 		this.edad = edad;
+	}
+	
+	public static int devuelveProximoID() {
+		return numeroEmpleado + 1;
 	}
 }
--- TP1_Grupo7_L4/src/ejercicio1/Principal.java
+++ TP1_Grupo7_L4/src/ejercicio1/Principal.java
@@ -7,7 +7,13 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Empleado emp = new Empleado();
+		Empleado emp2 = new Empleado("Ignacio",33);
+		
+		System.out.println(emp);
+		System.out.println(emp2);
+		
+		
 	}
 
 }
